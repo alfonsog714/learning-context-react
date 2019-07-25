738ad8f6cebbd68f54408f88452d2e473d537b60
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-25 12:26:46 -0700

    Added more elements to the form and added styling

--- src/Form.js
+++ src/Form.js
@@ -23,7 +23,42 @@ class Form extends Component {
     const { classes } = this.props;
     return (
       <main className={classes.main}>
-        <h1>FORM!</h1>
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">Sign In</Typography>
+          <Select value="english">
+            <MenuItem value="english">English</MenuItem>
+            <MenuItem value="spanish">Spanish</MenuItem>
+          </Select>
+          <form className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <Input id="email" name="email" autoFocus type="email" />
+            </FormControl>
+
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <Input id="password" name="password" autoFocus type="password" />
+            </FormControl>
+
+            <FormControlLabel
+              control={<Checkbox color="primary" />}
+              label="Remember Me"
+            />
+
+            <Button
+              variant="contained"
+              type="submit"
+              fullWidth
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+          </form>
+        </Paper>
       </main>
     );
   }
