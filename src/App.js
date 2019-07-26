b4d8f2c2ad4f572d9db797993aa236b400e6c46d
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-26 07:48:32 -0700

    Refactored App.js to be a functional component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "./Navbar.js";
 import Form from "./Form.js";
 import PageContent from "./PageContent.js";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
-class App extends Component {
-  render() {
-    return (
-      <ThemeProvider>
-        <LanguageProvider>
-          <PageContent>
-            <Navbar />
-            <Form />
-          </PageContent>
-        </LanguageProvider>
-      </ThemeProvider>
-    );
-  }
+export default function App() {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
 }
-
-export default App;
