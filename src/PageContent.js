b8ddc31ebf410c09c8b9ef0163a9e9bc01e3b241
Alfonso <alfonso.garcia2324@gmail.com>
2019-07-26 07:46:57 -0700

    Refactored PageContent to use hooks

--- src/PageContent.js
+++ src/PageContent.js
@@ -1,16 +1,28 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "./contexts/ThemeContext.js";
 
-export default class PageContent extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkMode } = this.context;
+export default function PageContent(props) {
+  const { isDarkMode } = useContext(ThemeContext);
 
-    const styles = {
-      backgroundColor: isDarkMode ? "black" : "white",
-      height: "100vh",
-      width: "100vw"
-    };
-    return <div style={styles}>{this.props.children}</div>;
-  }
+  const styles = {
+    backgroundColor: isDarkMode ? "black" : "white",
+    height: "100vh",
+    width: "100vw"
+  };
+
+  return <div style={styles}>{props.children}</div>;
 }
+
+// export default class PageContent extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { isDarkMode } = this.context;
+
+//     const styles = {
+//       backgroundColor: isDarkMode ? "black" : "white",
+//       height: "100vh",
+//       width: "100vw"
+//     };
+//     return <div style={styles}>{this.props.children}</div>;
+//   }
+// }
